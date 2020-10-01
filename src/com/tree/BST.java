47bac8fe22cca2f426edbd1e95a88ce8cc1fb3e3
Sandeepdhungana <70063857+Sandeepdhungana@users.noreply.github.com>
2020-10-01 09:48:45 +0530

    Update BST.java

--- src/com/tree/BST.java
+++ src/com/tree/BST.java
@@ -3,44 +3,45 @@ package com.tree;
 public class BST {
 Node root;
 	public BST() {
-		root=null;
+		root = null;
 	}
-
-	public Node NodeCreate(int value){
+// 	this function returns the new node by settingvalue to value and child to be null.
+	public Node NodeCreate(int value) {
 		return new Node(value, null, null);
 	}
-	public void add(Node start, Node newNode){
-		if(root==null){
-			root=newNode;
+// 	function to add the elements in BST.
+	public void add(Node start, Node newNode) {
+		if(root == null){
+			root = newNode;
 			return;
 		}
-		if(newNode.value> start.value){
-			if( start.right==null)
-				start.right=newNode;
-			add(start.right,newNode);
+		if(newNode.value > start.value) {
+			if( start.right == null)
+				start.right = newNode;
+			add(start.right, newNode);
 		}
-		if(newNode.value< start.value){
-			if( start.left==null)
-				start.left=newNode;
-			add(start.left,newNode);
+		if(newNode.value < start.value) {
+			if( start.left == null)
+				start.left = newNode;
+			add(start.left, newNode);
 		}
 	}
-
-public void Search(int value, Node start){
+// function to search the given value using recursion.
+public void Search(int value, Node start) {
 	
-	if(start==null){
-		System.out.println("node isnot found");
+	if(start == null) {
+		System.out.println("node is not found");
 		return;
 	}
-	if(start.value==value)
+	if(start.value == value)
 	{
 		System.out.println("node is found");
 		return;
 	}
-	if( value>start.value){
+	if( value > start.value) {
 		Search(value, start.right);
 	}
-	if( value<start.value){
+	if( value < start.value) {
 		Search(value, start.left);
 	}
 }

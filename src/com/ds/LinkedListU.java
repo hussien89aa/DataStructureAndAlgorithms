a69480ff807b8d46fcb9e2a02c721ba8d3b395f5
Sandeepdhungana <70063857+Sandeepdhungana@users.noreply.github.com>
2020-10-01 09:53:43 +0530

    Update LinkedListU.java

--- src/com/ds/LinkedListU.java
+++ src/com/ds/LinkedListU.java
@@ -3,26 +3,29 @@ package com.ds;
 public class LinkedListU<T> {
  Node head;
 	public LinkedListU() {
-	 head= null;
+	 head = null;
 	}
-public void add(Object value){
-	Node  newNode=new Node(value, null);
-	if(head==null)
-		head=newNode;
-	else{
+// 	function to add value of type object and its children in linked list.
+public void add(Object value) {
+	Node  newNode = new Node(value, null);
+	if(head == null)
+		head = newNode;
+	else {
 		newNode.next=head;
-		head= newNode;
+		head = newNode;
 	}
 }
+// 	function to delete node of linked list.
 public void delete(){
-	head=head.next;
+	head = head.next;
 	
 }
+// 	function to display elements of the linked list.
 public void display(){
-	Node n=head;
-	while(n!=null){
+	Node n = head;
+	while(n != null){
 		System.out.println((T)n.value);
-		n=n.next;
+		n = n.next;
 	}
 }
 
